d5474a76384f105e2f17528cb9b0fc7679d74c2a
arunarora-kiwibank <111322477+arunarora-kiwibank@users.noreply.github.com>
2023-04-11 03:48:07 +1200

    Updated error messages for validation

--- TowerCardApi/Core/Validators/CardInformationValidator.cs
+++ TowerCardApi/Core/Validators/CardInformationValidator.cs
@@ -11,19 +11,32 @@ public class CardInformationValidator : AbstractValidator<CardInformation>
             .NotEmpty()
             .MaximumLength(50);
 
+        var nullErrorMessage = "'{PropertyName}' must have a value.";
+        var numericErrorMessage = "'{PropertyName}' must be Numeric.";
+        var lengthErrorMessage = "'{PropertyName}' must be {Length} digits.";
+
         RuleFor(x => x.CardNumber)
             .NotEmpty()
+            .WithMessage(nullErrorMessage)
             .Must(value => value.ToCharArray().All(char.IsDigit))
-            .Length(16);
+            .WithMessage(numericErrorMessage)
+            .Length(16)
+            .WithMessage(lengthErrorMessage);
 
         RuleFor(x => x.Cvc)
             .NotEmpty()
+            .WithMessage(nullErrorMessage)
             .Must(value => value.ToCharArray().All(char.IsDigit))
-            .Length(3);
+            .WithMessage(numericErrorMessage)
+            .Length(3)
+            .WithMessage(lengthErrorMessage);
 
         RuleFor(x=> x.ExpiryDate)
             .NotEmpty()
+            .WithMessage(nullErrorMessage)
             .Must(value => value.ToCharArray().All(char.IsDigit))
-            .Length(4);
+            .WithMessage(numericErrorMessage)
+            .Length(4)
+            .WithMessage(lengthErrorMessage);
     }
 }
