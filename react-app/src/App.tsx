e94ba6666aa44b19b614703c880f7422191273e7
arunarora-kiwibank <111322477+arunarora-kiwibank@users.noreply.github.com>
2023-04-11 01:33:21 +1200

    Bug fix with navigation bar

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import RegisterCard from './pages/card/RegisterCard';
+import { MenuOpenContext } from './context/MenuContext';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <div className="App">
-      <NavBar />
-      <RegisterCard />
+      <MenuOpenContext.Provider value={{ menuOpen, setMenuOpen }}>
+        <NavBar />
+        <RegisterCard />
+      </MenuOpenContext.Provider>
     </div>
   );
 }
