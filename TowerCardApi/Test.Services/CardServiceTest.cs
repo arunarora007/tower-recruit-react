c2086a69a9dea57cd6a7f1d594233ccaaef6d0b8
arunarora-kiwibank <111322477+arunarora-kiwibank@users.noreply.github.com>
2023-04-11 02:38:23 +1200

    Added Unit Test for CardService

---
+++ TowerCardApi/Test.Services/CardServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using TowerCardApi.Core.Entities;
+using TowerCardApi.Core.Interfaces;
+using TowerCardApi.Services.Cache;
+using TowerCardApi.Services.Services;
+
+namespace TowerCardApi.Test.Services;
+
+[TestClass]
+public class CardServiceTest
+{
+    private readonly ICardService repo = new CardService();
+
+    [TestMethod]
+    public void Test_AddCardEntity_Passing()
+    {
+        var entity = new CardEntity()
+        {
+             Id = Guid.NewGuid(),
+              CardNumber = "1234123412341234",
+              Cvc = "123",
+              ExpiryDate = "1256",
+              Name = "Test"
+        };
+
+        repo.Create(entity);
+
+
+        CollectionAssert.Contains(InMemoryStorage.cards.ToList(), entity);
+    }
+}
