d39ac31fdf8f1c6d66e9d433338bd4295759ec14
arunarora-kiwibank <111322477+arunarora-kiwibank@users.noreply.github.com>
2023-04-11 00:34:57 +1200

    Adding validation to UI

--- react-app/src/pages/card/CardForm.tsx
+++ react-app/src/pages/card/CardForm.tsx
@@ -2,11 +2,33 @@ import { Button, Grid, TextField } from "@mui/material"
 import { useState } from "react"
 import { PatternFormat } from "react-number-format"
 import { CardInformation } from "../../model"
+import { validateCardNumber, validateCvc, validateExpiryDate } from "../../validations/Validate"
 
 const CardForm = () => {
     const [cardNumber, setCardNumber] = useState<string>('')
     const [cvc, setCvc] = useState<string>('')
     const [expiryDate, setExpiryDate] = useState<string>('')
+    const [cardNumberError, setCardNumberError] = useState<string>('')
+    const [cvcError, setCvcError] = useState<string>('')
+    const [expiryDateError, setExpiryDateError] = useState<string>('')
+    
+    const cardNumberBlur = () => {
+        setCardNumberError('')
+        const error = validateCardNumber(cardNumber)
+        if (error) setCardNumberError(error)
+    }
+
+    const cvcBlur = () => {
+        setCvcError('')
+        const error = validateCvc(cvc)
+        if (error) setCvcError(error)
+    }
+
+    const expiryDateBlur = () => {
+        setExpiryDateError('')
+        const error = validateExpiryDate(expiryDate)
+        if (error) setExpiryDateError(error)
+    }
     
     const registerCard = (body: CardInformation) => {
         console.log(body)
@@ -41,6 +63,9 @@ const CardForm = () => {
                         autoFocus
                         fullWidth
                         valueIsNumericString
+                        onBlur={cardNumberBlur}
+                        error={cardNumberError ? true : false}
+                        helperText={cardNumberError}
                     />
                 </Grid>
 
@@ -59,6 +84,9 @@ const CardForm = () => {
                             }}
                             required
                             valueIsNumericString
+                            onBlur={cvcBlur}
+                            error={cvcError ? true : false}
+                            helperText={cvcError}
                         />
                     </Grid>
 
@@ -76,6 +104,9 @@ const CardForm = () => {
                             }}
                             required
                             valueIsNumericString
+                            onBlur={expiryDateBlur}
+                            error={expiryDateError ? true : false}
+                            helperText={expiryDateError}
                         />
                     </Grid>
                 </Grid>
