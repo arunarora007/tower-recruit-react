6bc64f71b170a9652541249f66e9d7095b450db3
arunarora-kiwibank <111322477+arunarora-kiwibank@users.noreply.github.com>
2023-04-11 03:19:44 +1200

    Added validations for Card information

---
+++ TowerCardApi/Core/Validators/CardInformationValidator.cs
@@ -0,0 +1,29 @@
+﻿using TowerCardApi.Core.Models;
+using FluentValidation;
+
+namespace TowerCardApi.Core.Validators;
+
+public class CardInformationValidator : AbstractValidator<CardInformation>
+{
+    public CardInformationValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.CardNumber)
+            .NotEmpty()
+            .Must(value => value.ToCharArray().All(char.IsDigit))
+            .Length(16);
+
+        RuleFor(x => x.Cvc)
+            .NotEmpty()
+            .Must(value => value.ToCharArray().All(char.IsDigit))
+            .Length(3);
+
+        RuleFor(x=> x.ExpiryDate)
+            .NotEmpty()
+            .Must(value => value.ToCharArray().All(char.IsDigit))
+            .Length(4);
+    }
+}
