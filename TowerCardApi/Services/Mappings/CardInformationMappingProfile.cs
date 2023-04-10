e818f9498e58cd6d86a7c2d8ef555836bad660c9
arunarora-kiwibank <111322477+arunarora-kiwibank@users.noreply.github.com>
2023-04-11 02:53:13 +1200

    Added Automapper and Mapping Profile for Card Information

---
+++ TowerCardApi/Services/Mappings/CardInformationMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TowerCardApi.Core.Entities;
+using TowerCardApi.Core.Models;
+
+namespace TowerCardApi.Services.Mappings;
+
+public class CardInformationMappingProfile : Profile
+{
+    public CardInformationMappingProfile()
+    {
+        CreateMap<CardInformation, CardEntity>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.CardNumber, opt => opt.MapFrom(src => src.CardNumber))
+            .ForMember(dest => dest.Cvc, opt => opt.MapFrom(src => src.Cvc))
+            .ForMember(dest => dest.ExpiryDate, opt => opt.MapFrom(src => src.ExpiryDate))
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()));
+    }
+}
